d1c81bd44525d33a1bb6df9b5a46976d8f149227
Heath Farrow <heath.farrow@gmail.com>
2015-01-18 11:30:43 -0800

    Added example for nlog

---
+++ Assets/Ghostbit/TweakerUnity/Examples/SimpleExample.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ghostbit.Tweaker.Core;
+using NLog;
+using NLog.Config;
+using System.IO;
+using System.Xml;
+using NLog.Targets;
+using Ghostbit.Framework.Unity.Utils;
+
+public class SimpleExample : MonoBehaviour
+{
+    private Logger logger;
+    private Tweaker tweaker;
+
+    public static void Log(string level, string logger, string message)
+    {
+        Debug.Log(level + "|" + logger + "|" + message);
+    }
+    
+    void Start()
+    {
+        // Set up logging (required by Tweaker)
+        TextAsset config = Resources.Load<TextAsset>("NLog-Simple.config");
+        StringReader sr = new StringReader(config.text);
+        XmlReader xr = XmlReader.Create(sr);
+        LogManager.Configuration = new XmlLoggingConfiguration(xr, null); 
+        logger = LogManager.GetCurrentClassLogger();
+
+        tweaker = new Tweaker();
+        tweaker.Initialized += OnInitialized;
+        tweaker.Init();
+    }
+
+    private void OnInitialized()
+    {
+        logger.Info("OnInitialized");
+        tweaker.Shutdown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
